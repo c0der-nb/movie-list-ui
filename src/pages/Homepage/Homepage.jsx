5ca63457a3256eabc44bf8a3b1b545498873343d
Aditya Gupta <gaditya060@gmail.com>
2024-06-11 20:35:15 +0530

    Add pagination

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -7,18 +7,45 @@ import { getMovieList, searchMovie } from '../../api/api';
 function Homepage() {
     const [movieList, setMovieList] = useState([]);
     const [pageCount, setPageCount] = useState(1);
+    const [isLoading, setIsLoading] = useState(false);
 
-    useEffect(() => {
+    const nextHandler = () => {
+        setPageCount((count) => count+1);
+    }
+
+    const prevhandler = () => {
+        if (pageCount > 1) {
+            setPageCount((count) => count-1);
+        }
+    }
+
+    const loadData = () => {
+        setIsLoading(true);
         getMovieList(pageCount).then((data) => setMovieList(data.results))
                                .catch((err) => console.log(err));
-    }, []);
+        setIsLoading(false);
+    }
+
+    useEffect(() => {
+        loadData();
+    }, [pageCount]);
 
     return (
         <div className={styles.wrapper}>
-            <Navbar />
+            <div className={styles.navbar}>
+                <Navbar />
+            </div>
+            {!isLoading ?
             <div className={styles['card-container']}>
                 {movieList.map((movie) => (<MovieCard data={movie} />))}
+            </div> : <div className={styles.loaderContainer}><div className='loader'></div></div>}
+            <div className={styles.paginationContainer}></div>
+            <div className={styles.pagination}>
+                <button onClick={prevhandler} className={styles.buttonPrevNext}>Previous</button>
+                <button className={styles.pageNumView}>{pageCount}</button>
+                <button onClick={nextHandler} className={styles.buttonPrevNext}>Next</button>
             </div>
+            <footer></footer>
         </div>
     )
 }
